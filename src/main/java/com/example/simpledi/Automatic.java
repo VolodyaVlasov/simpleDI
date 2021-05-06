9d9b1d60da5375f414113304c4ba3b92fd2f052d
volodyavlasov <ivlasovvladimir@gmail.com>
2021-05-06 21:35:36 +0300

    update

--- src/main/java/com/example/simpledi/Automatic.java
+++ src/main/java/com/example/simpledi/Automatic.java
@@ -1,5 +1,9 @@
 package com.example.simpledi;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Automatic implements Gun{
     @Override
     public String shoot() {
--- src/main/java/com/example/simpledi/Gunslinger.java
+++ src/main/java/com/example/simpledi/Gunslinger.java
@@ -3,9 +3,7 @@ package com.example.simpledi;
 
 import com.example.simpledi.annotation.InjectRandomInt;
 import com.example.simpledi.aop.ChangeName;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
@@ -13,20 +11,16 @@ import javax.annotation.PreDestroy;
 public class Gunslinger {
     @InjectRandomInt(min = 1, max = 3)
     private int repeat;
-    @Qualifier(value = "pistol")
     private final Gun leftHand;
-    @Qualifier(value = "automatic")
     private final Gun rightHand;
-    private final String name = "Roland";
-
+    private final String name;
 
     public Gunslinger(Gun leftHand, Gun rightHand) {
         System.out.println("constructor");
         this.leftHand = leftHand;
         this.rightHand = rightHand;
-        //this.name = "Roland";
+        this.name = "Roland";
     }
-
 
     public void shoot() {
         for (int i = 0; i < repeat; i++) {
@@ -35,12 +29,10 @@ public class Gunslinger {
         }
     }
 
-
     @ChangeName
     public String getName() {
         return name;
     }
-
 
     @PostConstruct
     public void post() {
